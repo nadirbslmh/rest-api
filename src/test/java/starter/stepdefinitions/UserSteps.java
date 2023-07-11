961b0e84b630b097b7e36b91f38d858999fb1610
nadirbasalamah <nadir008basalamah@gmail.com>
2023-07-11 15:58:43 +0700

    fix(test): remove unused import

--- src/test/java/starter/stepdefinitions/UserSteps.java
+++ src/test/java/starter/stepdefinitions/UserSteps.java
@@ -4,7 +4,6 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import net.thucydides.core.annotations.Step;
 import net.thucydides.core.annotations.Steps;
 import starter.user.Delete;
 import starter.user.Get;
